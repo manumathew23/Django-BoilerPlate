6db18735253ece855739d1354f7e0469edce6740
Manu Mathew <manumathew@Manus-MacBook-Air.local>
2021-08-25 03:08:54 +0530

    fixes and res data point addition

--- favorites/utilities.py
+++ favorites/utilities.py
@@ -14,18 +14,21 @@ def fetch_data(category: str, item_id: int=None) -> list:
 
 	"""
 
-	next = True
+	fetch = True
 	results = []
 	url = SWAPI_URL + category
 	if item_id:
 		url += '/%s/' % str(item_id)
 
-	while next:
+	while fetch:
 		res = json.loads(requests.get(url).content)
+
 		if item_id:
 			return [res]
+
 		results += res.get("results", [])
-		next = url = res.get("next")
+		# fetch flag determines if additional records are to be fetched
+		fetch = url = res.get("next")
 
 	return results
 
@@ -37,8 +40,10 @@ def get_customization(user_id: int, category: str, search_key: str) -> dict:
 	for the given category for a user
 
 	"""
+
 	if not user_id:
 		return {}
+
 	table = Customization.objects.filter(
 		user_id=user_id,
 		category=CATEGORY_ID_MAP.get(category)
@@ -53,6 +58,12 @@ def get_customization(user_id: int, category: str, search_key: str) -> dict:
 
 	return custom_map
 
+def add_additional_fields(item_dict, item, category):
+    item_dict["created"] = item.get("created")
+    item_dict["updated"] = item.get("edited")
+    item_dict["url"] = item.get("url")
 
+    if category == "films":
+        item_dict["relased_at"] = item.get("released_at")
 
-
+    return item_dict
--- favorites/views.py
+++ favorites/views.py
@@ -3,14 +3,14 @@ from rest_framework.views import APIView
 
 from .models import Customization
 from .constants import CATEGORY_ID_MAP
-from .utilities import fetch_data, get_customization
+from .utilities import fetch_data, get_customization, add_additional_fields
 
 
 class CommonView(APIView):
 
     def get(self, request, category, item_id=None):
         """
-        Display a list of `<category>` (plantes / films / people/ starships)
+        Display a list of `<category>` items (planets / films / people / starships)
         from swapi API with is_favorite flag and custom_name, if any
         Optionally performs search on name and return details of item_id alone
 
@@ -18,49 +18,51 @@ class CommonView(APIView):
              - `search_key`: <str>, <optional>
              - `category`: <str>, <required>
              - `item_id`: <str>, <optional>
-
         """
 
-        response = []
+        response = list()
+
         user_id = request.GET.get("user_id", None)
         search_key = request.GET.get("search_key", "")
+
+        # Fetch swapi data for the category items or details of item_id
         data = fetch_data(category, item_id)
+
+        # Get custom name and is_favorite mapping for the category for the user
         custom_map = get_customization(user_id, category, search_key)
 
         for item in data:
-            name, is_favorite = custom_map.get(
+            item_dict = dict()
+            # id of an item is available as the last part of url
+            item_dict["name"], item_dict["is_favorite"] = custom_map.get(
                 int(item.get("url").split("/")[-2]),
                 (item.get("name"), False)
             )
 
-            if search_key and search_key not in name:
+            if search_key and search_key not in item_dict["name"]:
                 continue
 
-            response.append({
-                "name": name,
-                "created": item.get("created"),
-                "updated": item.get("edited"),
-                "url": item.get("url"),
-                "is_favorite": is_favorite,
-            })
+            response.append(add_additional_fields(item_dict, item, category))
 
         return HttpResponse(response)
+
 
     def post(self, request, category, item_id):
         """ To add an item (plantes / films / people/ starships) as favorite
         and allow setting a custom title/name to an item
 
         :params
+            - `name`: <str>, <optional>
             - `user_id`: <int>, <required>
             - `item_id`: <int>, <required>
-            - `name`: <str>, <optional>
             - `category`: <str>, <required>
             - `is_favorite`: <str>, <optional>
 
         """
+
         data = request.data
-        user_id = data.get("user_id")
         name = data.get("name", "")
+        user_id = data.get("user_id")
 
         custom_obj, _ = Customization.objects.get_or_create(
             user_id=user_id, item_id=item_id, category=CATEGORY_ID_MAP.get(category)
